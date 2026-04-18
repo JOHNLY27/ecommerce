81fbd667d6e048449bf144d7121770da9d484c7a
Johnlydrick <johnlydrick.remitar@urios.edu.ph>
2026-04-19 01:58:47 +0800

    Implement background queuing for emails to eliminate checkout and login lag

--- backend/app/Mail/OrderConfirmationMail.php
+++ backend/app/Mail/OrderConfirmationMail.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class OrderConfirmationMail extends Mailable
+class OrderConfirmationMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
